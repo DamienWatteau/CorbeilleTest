62bb0c4196467962ec8aefeae5480dc8341dcf93
antoi <antoi@10.64.128.25>
2017-06-02 09:33:18 +0200

    com

--- Corbeille1/src/jpu2016/dogfight/model/DogfightModel.java
+++ Corbeille1/src/jpu2016/dogfight/model/DogfightModel.java
@@ -15,8 +15,6 @@ public class DogfightModel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("LOL");
-        //damien mange des patates
-        //TEAMJULIA
+        System.out.println("");
     }
 }
