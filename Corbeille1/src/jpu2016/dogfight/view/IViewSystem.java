1f38855755a8755c7569104f274b419d4f01af22
antoi <antoi@10.64.128.25>
2017-06-02 10:08:48 +0200

    com

---
+++ Corbeille1/src/jpu2016/dogfight/view/IViewSystem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpu2016.dogfight.view;
+
+/**
+ *
+ * @author antoi
+ */
+public interface IViewSystem {
+    public displayMessage(String message){
+        
+    }
+    public void closeAll(){
+        
+    }
+}
