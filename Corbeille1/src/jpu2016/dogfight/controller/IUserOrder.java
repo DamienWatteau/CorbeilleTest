d5f9adcd4759f26ce572a1bc900609830945d566
Elderion64 <antoine.barachet@viacesi.fr>
2017-06-02 11:32:54 +0200

    classe controller

---
+++ Corbeille1/src/jpu2016/dogfight/controller/IUserOrder.java
@@ -0,0 +1,11 @@
+package jpu2016.dogfight.controller;
+
+public interface IUserOrder{
+	
+	
+	public Order getOrder();
+	
+	public int getPlayer();
+	
+	
+}
---
+++ Corbeille1/src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,24 @@
+package jpu2016.dogfight.controller;
+
+
+
+public class UserOrder implements IUserOrder{
+	private Order order;
+	
+	private int player;
+	
+	public UserOrder(int player, Order order){
+		
+	}
+	
+	public Order getOrder(){
+		return order;
+	}
+	
+
+	public int getPlayer() {
+		return player;
+	}
+
+	
+}
