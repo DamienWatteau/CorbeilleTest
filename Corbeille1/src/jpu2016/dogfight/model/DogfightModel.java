67fe75d0cfefcdf894bed550ea533cfa58d6d2bb
antoi <antoi@10.64.128.25>
2017-06-02 10:40:55 +0200

    com

--- Corbeille1/src/jpu2016/dogfight/model/DogfightModel.java
+++ Corbeille1/src/jpu2016/dogfight/model/DogfightModel.java
@@ -20,33 +20,25 @@ package jpu2016.dogfight.model;
             }
         protected sky sky;
 
-
     public IArea getArea(){
 
     }
-
     public void buildArea(Dimension dimension){
 
     }
-
     public void addMobile(IMobile Mobile){
 
     }
-
     public void removeMobile(IMobile Mobile){
 
     }
-
     public ArrayList<IMobile> getMobiles(){
 
     }
-
     public IMobile getMobileByPlayer(int player){
 
     }
-
     public void setMobilesHavesMoved(){
 
     }
-
 }
