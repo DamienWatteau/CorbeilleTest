0dea8d765721457b6f2f8870dcb816ea12550cd2
antoi <antoi@10.64.128.25>
2017-06-02 09:57:19 +0200

    com

---
+++ Corbeille1/src/jpu2016/dogfight/view/EventPerformer.java
@@ -0,0 +1,13 @@
+package jpu2016.dogfight.view;
+
+public class EventPerformer {
+    public EventPermormer(orderPerformer: IOrderPerformer){
+        
+    }
+    public void EventPerformer(keyCode: KeyEvent){
+        
+    }
+    private UserOrder keyCodeToUserOrder(int keyCode){
+        
+    }
+}
