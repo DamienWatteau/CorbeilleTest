fc79a651fe78900b4422731cceab91a7cea33512
antoi <antoi@10.64.128.25>
2017-06-02 11:45:07 +0200

    com

--- Corbeille1/src/jpu2016/gameframe/IGraphicsBuilder.java
+++ Corbeille1/src/jpu2016/gameframe/IGraphicsBuilder.java
@@ -4,4 +4,10 @@ public class IGraphicsBuilder {
     public void applyModelToGraphics(Graphics graphics, ImageObservable observable){
         
     }
+    public int getGlobalWidth(){
+               
+    }
+    public int getGlobalHeight(){
+                
+    }
 }
