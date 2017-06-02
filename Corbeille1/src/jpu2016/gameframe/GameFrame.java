f2e2ce5b749c09262c0674d42a24e1710ec1b403
antoi <antoi@10.64.128.25>
2017-06-02 11:31:18 +0200

    ajoue de la classe GameFrame & IEventPerformer

--- Corbeille1/src/jpu2016/gameframe/GameFrame.java
+++ Corbeille1/src/jpu2016/gameframe/GameFrame.java
@@ -1,5 +1,16 @@
 package jpu2016.gameframe;
 
 public class GameFrame {
-    
+    public GameFrame(String title, IEventPermormer performer, IGraphicsBuilder graphicBuilder, Observable observable){
+        
+    }
+    public void keyPresse(KeyEvent keyevent){
+        
+    }
+    public void keyReleased(KeyEvent keyCode){
+        
+    }
+    public void keyTyped(KeyEvent keyEvent){
+        
+    }
 }
---
+++ Corbeille1/src/jpu2016/gameframe/IEventPermormer.java
@@ -0,0 +1,7 @@
+package jpu2016.gameframe;
+
+public class IEventPermormer {
+    public void eventPerform(KeyEvent keyCode){
+        
+    }
+}
