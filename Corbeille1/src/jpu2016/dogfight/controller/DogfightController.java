d70dbaa6fbc0fe04102279888acbda571cf901f3
antoi <antoi@10.64.128.25>
2017-06-06 15:31:19 +0200

    com

--- Corbeille1/src/jpu2016/dogfight/controller/DogfightController.java
+++ Corbeille1/src/jpu2016/dogfight/controller/DogfightController.java
@@ -13,133 +13,133 @@ import jpu2016.dogfight.model.Position;
 import jpu2016.dogfight.view.IViewSystem;
 
 public class DogfightController implements IOrderPerformer {
-	private static int						TIME_SLEEP	= 30;
-	private final IDogfightModel	dogfightModel;
-	private boolean								isGameOver	= false;
-	private IViewSystem						viewSystem;
 
-	public DogfightController(final IDogfightModel dogfightModel) {
-		this.dogfightModel = dogfightModel;
-	}
+    private static int TIME_SLEEP = 30;
+    private final IDogfightModel dogfightModel;
+    private boolean isGameOver = false;
+    private IViewSystem viewSystem;
 
-	@Override
-	public void orderPerform(final IUserOrder userOrder) {
-		if (userOrder != null) {
-			final IMobile plane = this.dogfightModel.getMobileByPlayer(userOrder.getPlayer());
-			if (plane != null) {
-				Direction direction;
-				switch (userOrder.getOrder()) {
-					case UP:
-						direction = Direction.UP;
-						break;
-                                        case RIGHT:
-						direction = Direction.RIGHT;
-						break;
-                                        case LEFT:
-						direction = Direction.LEFT;
-						break;
-                                        case DOWN:
-						direction = Direction.DOWN;
-						break;
-					case SHOOT:
-                                {
-                                    try {
-                                        lauchMissile(userOrder.getPlayer());
-                                    } catch (IOException ex) {
-                                        Logger.getLogger(DogfightController.class.getName()).log(Level.SEVERE, null, ex);
-                                    }
-                                }
-					case NOP:
-					default:
-						direction = this.dogfightModel.getMobileByPlayer(userOrder.getPlayer()).getDirection();
-						break;
-				}
-				plane.setDirection(direction);
-			}
-		}
-	}
+    public DogfightController(final IDogfightModel dogfightModel) {
+        this.dogfightModel = dogfightModel;
+    }
 
-	private void lauchMissile(final int player) throws IOException {
-		final IMobile plane = this.dogfightModel.getMobileByPlayer(player);
-		if (plane != null) {
-			final Position position = new Position(plane.getPosition().getX() + ((plane.getWidth() - Missile.getWidthWithADirection(plane.getDirection())) / 2),
-					plane.getPosition().getY() + ((plane.getHeight() - Missile.getHeightWithADirection(plane.getDirection())) / 2));
-			this.dogfightModel.addMobile(new Missile(plane.getDirection(), position));
-			switch (plane.getDirection()) {
-				case UP:
-					//TODO mise a jour de la position de l'avion
-                                    position.setY(position.getY()+1);
-					break;
-                                case DOWN:
-					//TODO mise a jour de la position de l'avion
-                                    position.setY(position.getY()-1);
-					break;
-                                case RIGHT:
-					//TODO mise a jour de la position de l'avion
-                                    position.setX(position.getX()+1);
-					break;
-                                case LEFT:
-					//TODO mise a jour de la position de l'avion
-                                    position.setX(position.getX()-1);
-					break;
-				default:
-					break;
-			}
-		}
-	}
+    @Override
+    public void orderPerform(final IUserOrder userOrder) {
+        if (userOrder != null) {
+            final IMobile plane = this.dogfightModel.getMobileByPlayer(userOrder.getPlayer());
+            if (plane != null) {
+                Direction direction;
+                switch (userOrder.getOrder()) {
+                    case UP:
+                        direction = Direction.UP;
+                        break;
+                    case RIGHT:
+                        direction = Direction.RIGHT;
+                        break;
+                    case LEFT:
+                        direction = Direction.LEFT;
+                        break;
+                    case DOWN:
+                        direction = Direction.DOWN;
+                        break;
+                    case SHOOT: {
+                        try {
+                            lauchMissile(userOrder.getPlayer());
+                        } catch (IOException ex) {
+                            Logger.getLogger(DogfightController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    case NOP:
+                    default:
+                        direction = this.dogfightModel.getMobileByPlayer(userOrder.getPlayer()).getDirection();
+                        break;
+                }
+                plane.setDirection(direction);
+            }
+        }
+    }
 
-	private boolean isWeaponOnMobile(final IMobile mobile, final IMobile weapon) {
-		//TODO renvoie vrai si le missile touche l'élément mobile
-		return false;
-	}
+    private void lauchMissile(final int player) throws IOException {
+        final IMobile plane = this.dogfightModel.getMobileByPlayer(player);
+        if (plane != null) {
+            final Position position = new Position(plane.getPosition().getX() + ((plane.getWidth() - Missile.getWidthWithADirection(plane.getDirection())) / 2),
+                    plane.getPosition().getY() + ((plane.getHeight() - Missile.getHeightWithADirection(plane.getDirection())) / 2));
+            this.dogfightModel.addMobile(new Missile(plane.getDirection(), position));
+            switch (plane.getDirection()) {
+                case UP:
+                    //TODO mise a jour de la position de l'avion
+                    position.setY(position.getY() + 1);
+                    break;
+                case DOWN:
+                    //TODO mise a jour de la position de l'avion
+                    position.setY(position.getY() - 1);
+                    break;
+                case RIGHT:
+                    //TODO mise a jour de la position de l'avion
+                    position.setX(position.getX() + 1);
+                    break;
+                case LEFT:
+                    //TODO mise a jour de la position de l'avion
+                    position.setX(position.getX() - 1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
-	private void manageCollision(final IMobile weapon) {
-		final ArrayList<IMobile> target = new ArrayList<IMobile>();
-		boolean isTargetHit = false;
+    private boolean isWeaponOnMobile(final IMobile mobile, final IMobile weapon) {
+        //TODO renvoie vrai si le missile touche l'élément mobile
+        return false;
+    }
 
-		for (final IMobile mobile : this.dogfightModel.getMobiles()) {
-			if (this.isWeaponOnMobile(mobile, weapon)) {
-				target.add(mobile);
-			}
-		}
-		for (final IMobile mobile : target) {
-			isTargetHit = isTargetHit || mobile.hit(); //les avions renvoient vrai, les nuages et missiles renvoient faux
-		}
-		if (isTargetHit) {
-			this.dogfightModel.removeMobile(weapon);
-			this.isGameOver = true;
-		}
-	}
+    private void manageCollision(final IMobile weapon) {
+        final ArrayList<IMobile> target = new ArrayList<IMobile>();
+        boolean isTargetHit = false;
 
-	public void play() {
-		this.gameLoop();
-		this.viewSystem.displayMessage("Game Over !");
-		this.viewSystem.closeAll();
-	}
+        for (final IMobile mobile : this.dogfightModel.getMobiles()) {
+            if (this.isWeaponOnMobile(mobile, weapon)) {
+                target.add(mobile);
+            }
+        }
+        for (final IMobile mobile : target) {
+            isTargetHit = isTargetHit || mobile.hit(); //les avions renvoient vrai, les nuages et missiles renvoient faux
+        }
+        if (isTargetHit) {
+            this.dogfightModel.removeMobile(weapon);
+            this.isGameOver = true;
+        }
+    }
 
-	private void gameLoop() {
-		while (!this.isGameOver) {
-			try {
-				Thread.sleep(TIME_SLEEP);
-			} catch (final InterruptedException ex) {
-				Thread.currentThread().interrupt();
-			}
+    public void play() {
+        this.gameLoop();
+        this.viewSystem.displayMessage("Game Over !");
+        this.viewSystem.closeAll();
+    }
 
-			final ArrayList<IMobile> initialMobiles = new ArrayList<IMobile>();
-			for (final IMobile mobile : this.dogfightModel.getMobiles()) {
-				initialMobiles.add(mobile);
-			}
-			for (final IMobile mobile : initialMobiles) {
-				mobile.move();
-				if (mobile.isWeapon()) {
-					this.manageCollision(mobile);
-				}
-			}
-			this.dogfightModel.setMobilesHavesMoved();
-		}
-	}
+    private void gameLoop() {
+        while (!this.isGameOver) {
+            try {
+                Thread.sleep(TIME_SLEEP);
+            } catch (final InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
 
-	public void setViewSystem(final IViewSystem viewSystem) {
-		this.viewSystem = viewSystem;
-	}
+            final ArrayList<IMobile> initialMobiles = new ArrayList<IMobile>();
+            for (final IMobile mobile : this.dogfightModel.getMobiles()) {
+                initialMobiles.add(mobile);
+            }
+            for (final IMobile mobile : initialMobiles) {
+                mobile.move();
+                if (mobile.isWeapon()) {
+                    this.manageCollision(mobile);
+                }
+            }
+            this.dogfightModel.setMobilesHavesMoved();
+        }
+    }
+
+    public void setViewSystem(final IViewSystem viewSystem) {
+        this.viewSystem = viewSystem;
+    }
 }
--- Corbeille1/src/jpu2016/dogfight/view/EventPerformer.java
+++ Corbeille1/src/jpu2016/dogfight/view/EventPerformer.java
@@ -9,61 +9,62 @@ import jpu2016.dogfight.controller.UserOrder;
 import jpu2016.gameframe.IEventPerformer;
 
 class EventPerformer implements IEventPerformer {
-	private final IOrderPerformer orderPerformer;
 
-	public EventPerformer(final IOrderPerformer orderPerformer) {
-		this.orderPerformer = orderPerformer;
-	}
+    private final IOrderPerformer orderPerformer;
 
-	@Override
-	public void eventPerform(final KeyEvent keyCode) {
-		final IUserOrder userOrder = this.keyCodeToUserOrder(keyCode.getKeyCode());
-		if (userOrder != null) {
-			//TODO transmettre l'ordre au controleur (par son interface dédiée)
-                        orderPerformer.orderPerform(userOrder);
-			
-		}
-	}
+    public EventPerformer(final IOrderPerformer orderPerformer) {
+        this.orderPerformer = orderPerformer;
+    }
 
-	private IUserOrder keyCodeToUserOrder(final int keyCode) {
-		IUserOrder userOrder = null; //TESTTT
-		switch (keyCode) {
-			// ordres pour le joueur 1
-			case KeyEvent.VK_UP:
-				userOrder = new UserOrder(0, Order.UP);
-				break;
-			case KeyEvent.VK_RIGHT:
-				userOrder = new UserOrder(0, Order.RIGHT);
-				break;
-			case KeyEvent.VK_DOWN:
-				userOrder = new UserOrder(0, Order.DOWN);
-				break;
-			case KeyEvent.VK_LEFT:
-				userOrder = new UserOrder(0, Order.LEFT);
-				break;
-			case KeyEvent.VK_CONTROL: //touche control du clavier == shoot de l'avion
-				userOrder = new UserOrder(0, Order.SHOOT);
-				break;
+    @Override
+    public void eventPerform(final KeyEvent keyCode) {
+        final IUserOrder userOrder = this.keyCodeToUserOrder(keyCode.getKeyCode());
+        if (userOrder != null) {
+            //TODO transmettre l'ordre au controleur (par son interface dédiée)
+            orderPerformer.orderPerform(userOrder);
 
-			// ordres pour le joueur 2
-                        case KeyEvent.VK_Z:
-				userOrder = new UserOrder(1, Order.UP);
-				break;
-			case KeyEvent.VK_D:
-				userOrder = new UserOrder(1, Order.RIGHT);
-				break;
-			case KeyEvent.VK_S:
-				userOrder = new UserOrder(1, Order.DOWN);
-				break;
-			case KeyEvent.VK_Q:
-				userOrder = new UserOrder(1, Order.LEFT);
-				break;
-			case KeyEvent.VK_SPACE: //touche control du clavier == shoot de l'avion
-				userOrder = new UserOrder(1, Order.SHOOT);
-				break;        
-			default:
-				userOrder = null;
-		}
-		return userOrder;
-	}
+        }
+    }
+
+    private IUserOrder keyCodeToUserOrder(final int keyCode) {
+        IUserOrder userOrder = null; //TESTTT
+        switch (keyCode) {
+            // ordres pour le joueur 1
+            case KeyEvent.VK_UP:
+                userOrder = new UserOrder(0, Order.UP);
+                break;
+            case KeyEvent.VK_RIGHT:
+                userOrder = new UserOrder(0, Order.RIGHT);
+                break;
+            case KeyEvent.VK_DOWN:
+                userOrder = new UserOrder(0, Order.DOWN);
+                break;
+            case KeyEvent.VK_LEFT:
+                userOrder = new UserOrder(0, Order.LEFT);
+                break;
+            case KeyEvent.VK_CONTROL: //touche control du clavier == shoot de l'avion
+                userOrder = new UserOrder(0, Order.SHOOT);
+                break;
+
+            // ordres pour le joueur 2
+            case KeyEvent.VK_Z:
+                userOrder = new UserOrder(1, Order.UP);
+                break;
+            case KeyEvent.VK_D:
+                userOrder = new UserOrder(1, Order.RIGHT);
+                break;
+            case KeyEvent.VK_S:
+                userOrder = new UserOrder(1, Order.DOWN);
+                break;
+            case KeyEvent.VK_Q:
+                userOrder = new UserOrder(1, Order.LEFT);
+                break;
+            case KeyEvent.VK_SPACE: //touche control du clavier == shoot de l'avion
+                userOrder = new UserOrder(1, Order.SHOOT);
+                break;
+            default:
+                userOrder = null;
+        }
+        return userOrder;
+    }
 }
